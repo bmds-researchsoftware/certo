5b1120ff072d65fe9f26914ef637c87fa40a21ee
David J. Neu <david@davidneu.com>
2018-03-20 14:05:33 -0400

    Improved the sample data.

--- resources/db/study-subjects.clj
+++ resources/db/study-subjects.clj
@@ -1,14 +1,43 @@
-{:first_name "Martha" :last_name "Washington" :birth_date "1731-06-13" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Mary" :last_name "Smith" :birth_date "1980-06-13" :birth_state "VT" :children nil :created_by "djneu" :updated_by "djneu"}
 
-{:first_name "Abigail" :last_name "Adams" :birth_date "1744-11-22" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Patricia" :last_name "Johnson" :birth_date "1983-12-01" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu"}
 
-{:first_name "Martha" :last_name "Jefferson" :birth_date "1748-10-30" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Linda" :last_name "Williams" :birth_date "1980-07-02" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Barbara" :last_name "Jones" :birth_date "1979-05-02" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
 
-{:first_name "Betsy" :last_name "Ross" :birth_date "1752-01-01" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Elizabeth" :last_name "Brown" :birth_date "1976-03-17" :birth_state "ME" :children nil :created_by "djneu" :updated_by "djneu"}
 
-{:first_name "Dolly" :last_name "Madison" :birth_date "1768-05-20" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Jennifer" :last_name "Davis" :birth_date "1970-06-13" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Maria" :last_name "Miller" :birth_date "1980-06-13" :birth_state "VT" :children nil :created_by "djneu" :updated_by "djneu",} 
 
-{:first_name "Elizabeth" :last_name "Monroe" :birth_date "1768-06-30" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Susan" :last_name "Wilson" :birth_date "1989-06-13" :birth_state "CT" :children nil :created_by "djneu" :updated_by "djneu",} 
 
-{:first_name "Jackie" :last_name "Kennedy" :birth_date "1929-07-28" :birth_state nil :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Margaret" :last_name "Moore" :birth_date "1984-06-13" :birth_state "VT" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Dorothy" :last_name "Taylor" :birth_date "1989-06-13" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Lisa" :last_name "Anderson" :birth_date "1970-08-14" :birth_state "MA" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Nancy" :last_name "Thomas" :birth_date "1983-06-13" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Karen" :last_name "Johnston" :birth_date "1981-02-07" :birth_state "NJ" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Betty" :last_name "White" :birth_date "1983-04-29" :birth_state "ME" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Helen" :last_name "Harris" :birth_date "1970-06-13" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Sandra" :last_name "Martin" :birth_date "1982-09-11" :birth_state "VT" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Donna" :last_name "Thompson" :birth_date "1981-03-10" :birth_state "VA" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Carol" :last_name "Garcia" :birth_date "1970-02-23" :birth_state "CT" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Ruth" :last_name "Martinez" :birth_date "1984-05-14" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu",} 
+
+{:first_name "Sharon" :last_name "Robinson" :birth_date "1983-02-19" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu"}
+{:first_name "Michelle" :last_name "Clark" :birth_date "1978-06-13" :birth_state "ME" :children nil :created_by "djneu" :updated_by "djneu",} 
+{:first_name "Laura" :last_name "Rodriguez" :birth_date "1986-01-17" :birth_state "VT" :children nil :created_by "djneu" :updated_by "djneu"} 
+
+{:first_name "Sarah" :last_name "Lewis" :birth_date "1970-07-04" :birth_state "GA" :children nil :created_by "djneu" :updated_by "djneu"}
+
+{:first_name "Kimberly" :last_name "Lee" :birth_date "1981-08-17" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu"}
+
+{:first_name "Deborah" :last_name "Walker" :birth_date "1971-4-15" :birth_state "NH" :children nil :created_by "djneu" :updated_by "djneu"}
 
