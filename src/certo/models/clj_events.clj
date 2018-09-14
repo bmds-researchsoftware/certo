09a74d6fe733be2b3252f62d9650183df66e91f8
David J. Neu <david@davidneu.com>
2018-09-14 10:27:16 -0400

    Initial refactoring to add search to table views for SQL views.

---
+++ src/certo/models/clj_events.clj
@@ -0,0 +1,9 @@
+(ns certo.models.clj-events
+  (:require
+   [certo.sql-events :as sql-events])
+  (:import [java.util.UUID]))
+
+
+(defn sample-event [db params]
+  (throw (Exception. "certo.models.clj-events/sample-event is not implemented")))
+
